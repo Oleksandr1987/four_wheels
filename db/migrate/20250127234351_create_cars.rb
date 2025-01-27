acbda9e0c641346d0dd7acceec12bb4d53a53dea
Oleksandr Solovii <oleksandrsolovii@MBP-Oleksandr.netis>
2025-01-28 01:55:16 +0200

    Create car and user model

---
+++ db/migrate/20250127234351_create_cars.rb
@@ -0,0 +1,26 @@
+class CreateCars < ActiveRecord::Migration[7.2]
+  def change
+    create_table :cars do |t|
+      t.string :region
+      t.integer :year
+      t.integer :odometer
+      t.string :brand
+      t.string :model
+      t.string :state
+      t.decimal :price
+      t.decimal :engine_volume
+      t.string :fuel_type
+      t.string :gear_box
+      t.string :drive_type
+      t.string :color
+      t.string :vin_number
+      t.string :image_url
+      t.text :description
+      t.boolean :is_available
+      t.string :body_type
+      t.string :type
+
+      t.timestamps
+    end
+  end
+end
